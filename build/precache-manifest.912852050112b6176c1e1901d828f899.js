4f289081f1e3d63a450542f8e95217079aa17eb0
Kotelnikov Igor <digikot1996@gmail.com>
2019-11-25 20:05:01 +0200

    github page

---
+++ build/precache-manifest.912852050112b6176c1e1901d828f899.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de47de9bd4be3dd316e09cfebde5f500",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "8ed4bbd6a3707776ceab",
+    "url": "/goit-react-hw-02-reader/static/css/main.e2bf0b7b.chunk.css"
+  },
+  {
+    "revision": "cd6f2e74dc3614c71c91",
+    "url": "/goit-react-hw-02-reader/static/js/2.402b9108.chunk.js"
+  },
+  {
+    "revision": "8ed4bbd6a3707776ceab",
+    "url": "/goit-react-hw-02-reader/static/js/main.a31b6e66.chunk.js"
+  },
+  {
+    "revision": "fa8905c1df9e69eb6b4f",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.33dad756.js"
+  }
+]);
